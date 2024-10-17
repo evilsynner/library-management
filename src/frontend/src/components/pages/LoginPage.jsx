76176ee168e06450d93288652be98c8d31a5710a
evilsynner <168985262+evilsynner@users.noreply.github.com>
2024-10-16 21:01:25 -0500

    chore(backend/frontend): Added endpoints to obtain and update access tokens, login form now sends user credentials to the backend to obtain access tokens.

--- src/frontend/src/components/pages/LoginPage.jsx
+++ src/frontend/src/components/pages/LoginPage.jsx
@@ -2,33 +2,55 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const registrationData = {
-      email: email,
+    const loginData = {
+      username: username,
       password: password,
     };
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/token/", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(loginData)
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setUsername("");
+        setPassword("");
+        console.log(data);
+      } else {
+        const errorData = await response.json();
+        console.error(errorData);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
       <div className="bg-gray-800 p-8 rounded-lg shadow-md w-full max-w-sm relative">
         <h2 className="text-2xl font-bold text-white mb-6 text-center">Login into your account.</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-300 text-sm font-semibold mb-2" htmlFor="email">
-              Your email
+              Your username
             </label>
             <input
-              type="email"
-              id="email"
+              type="text"
+              id="username"
               className="w-full px-4 py-2 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="name@domain.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              placeholder="uname"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               required
             />
           </div>
